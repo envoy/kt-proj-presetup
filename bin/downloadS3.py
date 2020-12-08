357f1c1916696d32e49c4211551104f1e23672f9
GP <gp@envoy.com>
2020-12-08 10:13:11 -0800

    final changes

--- bin/downloadS3.py
+++ bin/downloadS3.py
@@ -4,11 +4,12 @@ import os
 
 BUCKET_NAME = os.environ['SSL_KEY_STORE_S3_BUCKET']
 KEY = os.environ['SSL_KEY_STORE_S3_FILENAME']
+OUTPUT_PATH = os.environ['SSL_KEY_STORE_DEST']
 
 s3 = boto3.resource('s3')
 
 try:
-    s3.Bucket(BUCKET_NAME).download_file(KEY, KEY)
+    s3.Bucket(BUCKET_NAME).download_file(KEY, OUTPUT_PATH)
 except botocore.exceptions.ClientError as e:
     if e.response['Error']['Code'] == "404":
         print("The object does not exist.")
